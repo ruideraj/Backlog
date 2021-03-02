46fe0c97e01fe0cedf75dcec189a90766d8be6b9
Jeremiah Ruidera <ruideraj@gmail.com>
2021-03-01 16:27:11 -0800

    Set theme colors Added icon resources Adjusted line character limit

---
+++ app/src/main/java/com/ruideraj/backlog/Models.kt
@@ -0,0 +1,29 @@
+package com.ruideraj.backlog
+
+import java.util.*
+
+enum class MediaType { FILM, SHOW, GAME, BOOK }
+
+enum class Status { TODO, IN_PROGRESS, DONE }
+
+enum class ListIcon { LIST, FILM, SHOW, GAME, BOOK }
+
+data class BacklogList(val listId: Long,
+                       val title: String,
+                       val icon: ListIcon,
+                       val position: Int,
+                       val count: Int)
+
+data class Entry(val entryId: Long,
+                 val listId: Long,
+                 val type: MediaType,
+                 val title: String,
+                 val metadata: Metadata,
+                 val status: Status)
+
+sealed class Metadata {
+    class FilmData(val director: String, releaseDate: Date) : Metadata()
+    class GameData(val developer: String, releaseDate: Date) : Metadata()
+    class ShowData(val showrunner: String, releaseDate: Date) : Metadata()
+    class BookData(val author: String, releaseDate: Date) : Metadata()
+}
